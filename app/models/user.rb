577e4c4ed88ba169a86e548f443b1dfd99562284
Alex Bunardzic <alexbunardzic@gmail.com>
2016-08-08 17:49:37 -0700

    adds user signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
 
   has_secure_password
-  vaidates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root to: 'visitors#index'
+  resource :users
 end
